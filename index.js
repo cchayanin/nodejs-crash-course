44d6f2eed3c4f313bd2281edd962f239c87741fb
cchay <cchayanin@gmail.com>
2022-02-03 16:56:47 +0700

    serve html with node

--- index.js
+++ index.js
@@ -9,9 +9,29 @@ function getPage(page) {
 
 http
   .createServer((req, res) => {
-    res.setHeader("Content-type", "text/html");
-    res.writeHead(200);
-    res.write(getPage("index.html"));
-    res.end();
+    const fileType = path.extname(req.url) || ".html";
+
+    if (fileType === ".html") {
+      res.setHeader("Content-type", "text/html");
+      res.writeHead(200);
+
+      if (req.url === "/") {
+        res.write(getPage("index.html"));
+      } else {
+        res.write(getPage(`${req.url}.html`));
+      }
+
+      res.end();
+    } else if (fileType === ".css") {
+      res.setHeader("Content-type", "text/css");
+      res.writeHead(200);
+
+      res.write(getPage(req.url));
+
+      res.end();
+    } else {
+      res.writeHead(404);
+      res.end;
+    }
   })
   .listen(3000);
